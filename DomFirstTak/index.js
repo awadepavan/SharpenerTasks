f1c2fa92748119773517d896016c339b56feb710
awadepavan <132901256+awadepavan@users.noreply.github.com>
2024-05-12 15:32:10 +0530

    first commit

---
+++ DomFirstTak/index.js
@@ -0,0 +1,19 @@
+// Write your code below:
+const main = document.getElementById("document.getElementById");
+main.textContent = "Fruit World";
+main.style.color = "orange";
+
+const header = document.getElementById("header");
+header.style.backgroundcolor = "green";
+header.style.borderBottom = "2px solid orange";
+
+
+const fruitsInBasket = document.getElementById("basket-heading");
+fruitsInBasket.style.color = "green";
+
+
+const thanksDiv = document.getElementById("thanks");
+const paragraph = document.createElement("p");
+paragraph.textContent = "Please visit us again";
+thanksDiv.appendChild(paragraph);
+
